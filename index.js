c20d4dcdd9339ffb898dd0a4f09d77195edb7fec
nums11 <numsmt2@gmail.com>
2018-09-16 10:42:43 -0400

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express ()
-
-const PORT = 3000
-
-app.get('/', (res, req)=> res.send("Fuck yeah"))
-app.listen(PORT, ()=> console.log("Server online on port %s", PORT))
